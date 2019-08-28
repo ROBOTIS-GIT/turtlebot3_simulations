7fd3379c78f1831c1f91c06e2d3b7dfb30bbfb32
Darby Lim <thlim@robotis.com>
2019-08-28 13:06:28 +0900

    Modifed licenses and use single-quotes

--- turtlebot3_gazebo/launch/robot_state_publisher.launch.py
+++ turtlebot3_gazebo/launch/robot_state_publisher.launch.py
@@ -1,20 +1,20 @@
-# /*******************************************************************************
-# * Copyright 2019 ROBOTIS CO., LTD.
-# *
-# * Licensed under the Apache License, Version 2.0 (the "License");
-# * you may not use this file except in compliance with the License.
-# * You may obtain a copy of the License at
-# *
-# *     http://www.apache.org/licenses/LICENSE-2.0
-# *
-# * Unless required by applicable law or agreed to in writing, software
-# * distributed under the License is distributed on an "AS IS" BASIS,
-# * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# * See the License for the specific language governing permissions and
-# * limitations under the License.
-# *******************************************************************************/
-
-# /* Author: Darby Lim */
+#!/usr/bin/env python3
+#
+# Copyright 2019 ROBOTIS CO., LTD.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# Authors: Darby Lim
 
 import os
 
@@ -31,7 +31,7 @@ def generate_launch_description():
     use_sim_time = LaunchConfiguration('use_sim_time', default='false')
     urdf_file_name = 'turtlebot3_' + TURTLEBOT3_MODEL + '.urdf'
 
-    print("urdf_file_name : {}".format(urdf_file_name))
+    print('urdf_file_name : {}'.format(urdf_file_name))
 
     urdf = os.path.join(
         get_package_share_directory('turtlebot3_description'),
