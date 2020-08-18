c8fbadaf8141b18a48dd566622da3c37aae2d7f9
ashe kim <ashekim@robotis.com>
2020-08-18 09:58:51 +0900

    modified lint

--- turtlebot3_gazebo/launch/turtlebot3_dqn_stage2.launch.py
+++ turtlebot3_gazebo/launch/turtlebot3_dqn_stage2.launch.py
@@ -22,7 +22,6 @@ from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.substitutions import ThisLaunchFileDir
 from launch.actions import ExecuteProcess
 from launch.substitutions import LaunchConfiguration
 
@@ -32,12 +31,14 @@ TURTLEBOT3_MODEL = os.environ['TURTLEBOT3_MODEL']
 def generate_launch_description():
     use_sim_time = LaunchConfiguration('use_sim_time', default='true')
     world_file_name = 'turtlebot3_dqn_stage2/' + TURTLEBOT3_MODEL + '.model'
-    world = os.path.join(get_package_share_directory('turtlebot3_gazebo'), 'worlds', world_file_name)
+    world = os.path.join(get_package_share_directory('turtlebot3_gazebo'),
+                         'worlds', world_file_name)
     launch_file_dir = os.path.join(get_package_share_directory('turtlebot3_gazebo'), 'launch')
 
     return LaunchDescription([
         ExecuteProcess(
-            cmd=['gazebo', '--verbose', world, '-s', 'libgazebo_ros_init.so', '-s', 'libgazebo_ros_factory.so'],
+            cmd=['gazebo', '--verbose', world, '-s', 'libgazebo_ros_init.so', '-s',
+                 'libgazebo_ros_factory.so'],
             output='screen'),
 
         IncludeLaunchDescription(
