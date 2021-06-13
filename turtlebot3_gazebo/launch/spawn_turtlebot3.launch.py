921344ffe287bdb1c0e1e9967ddc51cb19608e19
Will Son <willson@robotis.com>
2021-06-13 20:29:33 +0900

    fix flake8

--- turtlebot3_gazebo/launch/spawn_turtlebot3.launch.py
+++ turtlebot3_gazebo/launch/spawn_turtlebot3.launch.py
@@ -14,11 +14,11 @@
 
 import os
 
-from launch import LaunchDescription
-from launch.substitutions import LaunchConfiguration
-from launch.actions import DeclareLaunchArgument
-from launch_ros.actions import Node
 from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node
 
 
 def generate_launch_description():
