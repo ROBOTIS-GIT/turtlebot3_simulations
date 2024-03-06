8522e40d3950aab71f6620ca010c212f2a112fe1
SPC <simoncornelius16@gmail.com>
2024-03-06 17:27:14 +0800

    created launch for own world with top down camera image

--- turtlebot3_gazebo/launch/my_world.launch.py
+++ turtlebot3_gazebo/launch/my_world.launch.py
@@ -36,7 +36,7 @@ def generate_launch_description():
     world = os.path.join(
         get_package_share_directory('turtlebot3_gazebo'),
         'worlds',
-        'my_world.world'
+        'TB3_maze_simulation.world'
     )
 
     gzserver_cmd = IncludeLaunchDescription(
