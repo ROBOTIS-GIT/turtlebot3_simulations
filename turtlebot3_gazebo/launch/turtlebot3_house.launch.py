e62a5c8d22a267f473325236cc7101c14bd611c7
Hyungyu Kim <kimhg@robotis.com>
2025-03-25 02:17:55 +0000

    lint
    Signed-off-by: Hyungyu Kim <kimhg@robotis.com>

--- turtlebot3_gazebo/launch/turtlebot3_house.launch.py
+++ turtlebot3_gazebo/launch/turtlebot3_house.launch.py
@@ -20,15 +20,6 @@ import os
 
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription
-from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.substitutions import LaunchConfiguration
-
-
-import os
-
-from ament_index_python.packages import get_package_share_directory
-from launch import LaunchDescription
 from launch.actions import AppendEnvironmentVariable
 from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
