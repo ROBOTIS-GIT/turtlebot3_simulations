61d99c411a64aac750bd3c456884837693cbe535
Hyungyu Kim <kimhg@robotis.com>
2025-05-14 00:27:34 +0000

    change burger_cam physical attribution
    Signed-off-by: Hyungyu Kim <kimhg@robotis.com>

--- turtlebot3_gazebo/launch/spawn_turtlebot3.launch.py
+++ turtlebot3_gazebo/launch/spawn_turtlebot3.launch.py
@@ -54,7 +54,7 @@ def generate_launch_description():
             '-file', urdf_path,
             '-x', x_pose,
             '-y', y_pose,
-            '-z', '0.01',
+            '-z', '0.0',
             '-Y', yaw_pose
         ],
         output='screen',
