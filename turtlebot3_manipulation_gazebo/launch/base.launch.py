20785b446246c7ae4ecab79fb355c543922aa78e
chan-1207 <dddoggi1207@gmail.com>
2025-06-02 17:03:26 +0900

    Fix typo

--- turtlebot3_manipulation_gazebo/launch/base.launch.py
+++ turtlebot3_manipulation_gazebo/launch/base.launch.py
@@ -133,7 +133,7 @@ def generate_launch_description():
             ('~/cmd_vel_unstamped', 'cmd_vel'),
             ('~/odom', 'odom')
         ],
-        output="both",
+        output='both',
         condition=UnlessCondition(use_sim))
 
     robot_state_pub_node = Node(
